bfcf2cf84f6c9797145fe0ec47d105259d3c2fe0
felipebontempo <felipe.bontempo@gmail.com>
2020-06-01 14:15:24 -0300

    Criacao das classes modelos de entidades
    Classes que serão mapeadas pelo banco de dados.

---
+++ MinhasFinancas/Entidades/Movimentacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinhasFinancas.Entidades
+{
+    public class Movimentacao
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public Tipo Tipoy { get; set; } //É uma transação de entrada ou saída
+        public Usuario Usuario { get; set; }
+    }
+}
