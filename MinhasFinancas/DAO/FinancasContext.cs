34b9b000ceb6375d51236c74bb60e7dca08071ac
felipebontempo <felipe.bontempo@gmail.com>
2020-06-01 14:49:38 -0300

    Instacao do Entity Framework e preparacao do contexto do EF
    Foi criado o FinancasContext.
    
    linenotes

---
+++ MinhasFinancas/DAO/FinancasContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MinhasFinancas.Entidades;
+
+namespace MinhasFinancas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+        }
+    }
+}
