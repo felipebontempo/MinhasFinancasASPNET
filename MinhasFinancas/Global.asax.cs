a0c038cd244e3d4d659a193cd27a9e9dfd02d990
felipebontempo <felipe.bontempo@gmail.com>
2020-06-04 16:08:24 -0300

    Aplicando Segurança com o Simple Membership
    Para utilização do **Simples Mebership** foi preciso instalar esses dois
    pacotes:
    Install-Package Microsoft.AspNet.WebPages.WebData -Version 3.1.1
    Install-Package Microsoft.AspNet.WebPages.Data
    
    Essa foi apenas uma configuração inicial.
    
    linenote

--- MinhasFinancas/Global.asax.cs
+++ MinhasFinancas/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebMatrix.WebData;
 
 namespace MinhasFinancas
 {
@@ -16,6 +17,8 @@ namespace MinhasFinancas
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            WebSecurity.InitializeDatabaseConnection("FinancasContext", "Usuarios", "Id", "Nome", true);
         }
     }
 }
